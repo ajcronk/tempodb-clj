0570f71e455ec0bfcb3daf4a2dd0d7903afbaeca
Andrew J Cronk <andrew.cronk@gmail.com>
2012-08-19 09:45:54 -0500

    change def to let

--- src/tempodb_clj/client.clj
+++ src/tempodb_clj/client.clj
@@ -23,40 +23,34 @@
 
 (defn build-full-url
     [client target]
-    (def protocol (if (client :secure) 
-        "https://"
-        "http://"))
-    (format "%s%s:%s/%s%s" protocol (client :host) (client :port) constants/+tempodb-api-version+ target))
+    (let [protocol (if (client :secure) "https" "http")]
+        (format "%s://%s:%s/%s%s" protocol (client :host) (client :port) constants/+tempodb-api-version+ target)))
 
 (defn request
     [client target method & {:keys [body] :or {body nil}}]
-    (def http-call
-        (condp = method
-            "GET" http/get
-            "POST" http/post
-            "PUT" http/put))
-
-    (def url (build-full-url client target))
-    (println url)
-    (def result (http-call url 
+    (let [http-call
+            (condp = method
+                "GET" http/get
+                "POST" http/post
+                "PUT" http/put)
+          url (build-full-url client target)]
+        (println url)
+        (http-call url 
                     {:basic-auth [(client :key) (client :secret)]
                      :form-params body
                      :content-type :json
-                     :as :json  }))
-    result)
-
+                     :as :json  })))
 
 (defn create-series
     "create a new series with optionally specified key"
     [client & {:keys [key]
         :or {key nil}}]
-    (def target "/series/")
-    (def result 
-        (if key 
-            (request client target "POST" :body {:key key})
-            (request client target "POST")))
-
-    (result :body))
+    (let [target "/series/"
+          result 
+            (if key 
+                (request client target "POST" :body {:key key})
+                (request client target "POST"))]
+        (result :body)))
 
 
 (defn get-series
@@ -64,14 +58,15 @@
     [client & {:keys [id key tag attr]
         :or {id [] key [] tag [] attr {} }}]
 
-    (def target (format "/series/?%s" (get-querystring {:id id, :key key, :tag tag, :attr attr})))
-    (def result ((request client target "GET")))
-    (result :body))
+    (let [target (format "/series/?%s" (get-querystring {:id id, :key key, :tag tag, :attr attr}))
+          result (request client target "GET")]
+        (result :body)))
+    
 
 
 (defn update-series
     [client id key name tags attributes]
-    (def target (format "/series/id/%s/" id))
-    (def body {:id id, :key key, :name name, :tags tags, :attributes attributes})
-    (def result (request client target "PUT" :body body))
-    (result :body))
+    (let [target (format "/series/id/%s/" id)
+          body {:id id, :key key, :name name, :tags tags, :attributes attributes}
+          result (request client target "PUT" :body body)]
+        (result :body)))
--- src/tempodb_clj/examples/update_series.clj
+++ src/tempodb_clj/examples/update_series.clj
@@ -5,5 +5,5 @@
 (defn -main [& args]
     (def client (tempodb/new-client "your-api-key" "your-api-secret"))
     
-    (def s (tempodb/create-series client))
-    (tempodb/update-series client (s :id) (s :key) (s :name) ["tag1" "tag2"] {:foo "bar"}))
+    (let [s (tempodb/create-series client)]
+        (tempodb/update-series client (s :id) (s :key) (s :name) ["tag1" "tag2"] {:foo "bar"})))
